614f0b9bf9d6771f929d75c2c846ff2d924fc3a0
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-05-13 17:54:39 +0800

    add challenges 3

---
+++ chapter 01/challenges_03.c
@@ -0,0 +1,14 @@
+/* 
+ * Name: Challenge 03
+ * Purpose: To complete C Programming for the Absolute Beginner Challenge
+ * Author: Android Geeks
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Android Geeks\n");
+
+	return 0;
+}
