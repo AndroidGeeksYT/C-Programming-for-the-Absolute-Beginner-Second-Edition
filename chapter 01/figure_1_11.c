bd5ad086d831d60e4a69d8a0f861244a21e8c69a
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-05-13 12:45:16 +0800

    add figure 1.11

---
+++ chapter 01/figure_1_11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Chapter 1 - Getting Started with C Programming");
+	printf("This is an example of format bug.");
+	printf("The format issue can be corrected by using");
+	printf("the \n and \\ escape sequences\n");
+
+	return 0;
+}
