863db7894b80eca7c404a007f67c8e77ed75e86a
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-05-15 07:05:49 +0800

    add challenges 4

---
+++ chapter 01/challenges_04.c
@@ -0,0 +1,14 @@
+/* 
+ * Name: Challenge 04
+ * Purpose: To complete C Programming for the Absolute Beginner Challenge
+ * Author: Android Geeks
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\"Programming isn't about you know; it's about what you can figure out.\"\n");
+
+	return 0;
+}
