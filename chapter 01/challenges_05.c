3f90e6254eb9c4e2e06aa1215f38ea3824a270a9
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-05-15 16:37:58 +0800

    add challenges 5

---
+++ chapter 01/challenges_05.c
@@ -0,0 +1,14 @@
+/* 
+ * Name: Challenge 05
+ * Purpose: To complete C Programming for the Absolute Beginner Challenge
+ * Author: Android Geeks
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("c:\\cygwin\\home\\Administrator\n");
+
+	return 0;
+}
