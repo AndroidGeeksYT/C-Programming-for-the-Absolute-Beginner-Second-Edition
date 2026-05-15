6f23f8a3f1a757a1c6229711e201f3570a420802
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-05-15 17:41:22 +0800

    add challenges 7

---
+++ chapter 01/challenges_07.c
@@ -0,0 +1,21 @@
+/* 
+ * Name: Challenge 07
+ * Purpose: To complete C Programming for the Absolute Beginner Challenge
+ * Author: Android Geeks
+ */
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\nSun\tMon\tTue\tWed\tThu\tFri\tSat\n");
+	printf("\t\t\t\t\t1\t2\n");
+	printf("3\t4\t5\t6\t7\t8\t9\n");
+	printf("10\t11\t12\t13\t14\t15\t16\n");
+	printf("17\t18\t19\t20\t21\t22\t23\n");
+	printf("24\t25\t26\t27\t28\t29\t30\n");
+	printf("31\n");
+
+	return 0;
+}
