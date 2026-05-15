4cf0222445bc8199889d1c96c0e8d1fa2879d346
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-05-15 17:40:00 +0800

    add challenges 6

---
+++ chapter 01/challenges_06.c
@@ -0,0 +1,20 @@
+/* 
+ * Name: Challenge 06
+ * Purpose: To complete C Programming for the Absolute Beginner Challenge
+ * Author: Android Geeks
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("    *\n");
+	printf("  *   *\n");
+	printf(" *      *\n");
+	printf("*        *\n");
+	printf(" *      *\n");
+	printf("  *    *\n");
+	printf("     *\n");
+
+	return 0;
+}
