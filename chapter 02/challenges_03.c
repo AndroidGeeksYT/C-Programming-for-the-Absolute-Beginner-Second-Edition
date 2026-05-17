924312be8ca2aac644947fde5e8f31992408d5ac
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-05-17 12:15:48 +0800

    add challenges 3

---
+++ chapter 02/challenges_03.c
@@ -0,0 +1,19 @@
+/* 
+ * Name: Challenges 03
+ * Purpose: Store users name using scanf
+ * Author: Android Geeks
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char cUserName[20] = "\0";
+
+	printf("Username: ");
+	scanf("%s", &cUserName);
+
+	printf("Username: %s\n", cUserName);
+
+	return 0;
+}
