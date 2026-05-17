6e88aecefc30bc793b10d48dcaa6d5da244ddc89
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-05-17 09:22:01 +0800

    add challenges 1

---
+++ chapter 02/challenges_01.c
@@ -0,0 +1,16 @@
+/* 
+ * Name: Challenges 01
+ * Purpose: Create a program that outputs the given formula f = (a - b)(x - y)
+ * Author: Android Geeks
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 5, b = 1, x = 10, y = 5;
+
+	printf("The result is %d\n", (a - b) * (x - y));
+
+	return 0;
+}
